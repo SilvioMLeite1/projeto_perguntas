64017aed43d9562aef42808dab29395f009e54eb
SilvioMLeite1 <smarioleite@outlook.com>
2021-10-24 17:24:04 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,9 @@
+/* Aplicativos programa perguntas e respostas - Quiz v1.0
+ * Este material faz parte do curso de Flutter e tem como objetivo
+ * ensinar a programção em Flutter utilizando a linguagem Dart
+ * Aluno: Silvio Mario Leite
+ */
 
+main() {
+  print('Hello world pra dar sorte');
+}
